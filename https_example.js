68a8551dac889d790c98bd9351b3f6792fc220ed
Tony Kieling <tony.kieling@gmail.com>
2019-01-14 01:25:18 +0000

    coding done

--- https_example.js
+++ https_example.js
@@ -3,9 +3,13 @@ var https = require('https');
 // console.log('I did it!');
 
 
+// var options = {
+//   host: 'www.example.org',
+//   path: '/'
+// };
 var options = {
-  host: 'www.example.org',
-  path: '/'
+  host: 'stream-large-file.herokuapp.com',
+  path: '/give-me-stuff-now'
 };
 
 // called by https when the request is made.
